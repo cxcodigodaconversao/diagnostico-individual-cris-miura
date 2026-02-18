7caee6a579ee671d62692590f1afce0453840475
cxcodigodaconversao <cxcodigodaconversao@gmail.com>
2026-02-17 21:23:40 -0300

    Replace DiagnosticoCX with App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import DiagnosticoCX from './diagnosticoCX.jsx'
+import App from './App.jsx'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <DiagnosticoCX />
+    <App />
   </React.StrictMode>,
 )
