79f32ab3b4db6cd6e2a3f8830d28b1ef4d6966ac
cxcodigodaconversao <cxcodigodaconversao@gmail.com>
2026-02-17 21:13:03 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import DiagnosticoCX from './DiagnosticoCX.jsx'
+import DiagnosticoCX from './diagnosticoCX.jsx'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
